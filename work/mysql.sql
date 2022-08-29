8e819fffbfc38b3a783d239e4aa9f9786f7ca034
王立国 <qwasop@qq.com>
2022-08-29 14:08:49 +0800

    -

--- work/mysql.sql
+++ work/mysql.sql
@@ -4,7 +4,7 @@ CREATE TABLE `cron_config`  (
     `name` varchar(255) default '' COMMENT '任务名称',
     `spec` varchar(32) default '' COMMENT '执行时间，表达式',
     `protocol` tinyint(2) default 0 COMMENT '协议:1.http、2.grpc、3.系统命令行',
-    `command` varchar(255) default '' COMMENT '命令',
+    `command` json null COMMENT '命令',
     `remark` varchar(255) default '' COMMENT '备注',
     `status` tinyint(2) DEFAULT 1 COMMENT '状态：1.停止、2.启用',
     `create_dt` datetime NULL COMMENT '创建时间',
