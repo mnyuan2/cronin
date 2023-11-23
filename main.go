e06451bafebab679c1efb10eabfe45d5310d0dd0
王立国 <qwasop@qq.com>
2023-11-23 17:03:09 +0800

    增加版本号配置及展示

--- main.go
+++ main.go
@@ -4,14 +4,18 @@ import (
 	"cron/internal/basic/config"
 	"cron/internal/server"
 	"embed"
+	"fmt"
 )
 
 var (
 	//go:embed web
 	Resource embed.FS
+	version  = "" // 版本号  构建时通过 -ldflags "-X main.version=0.3.4" 进行指定
 )
 
 func main() {
+	config.Version = version
+	fmt.Println("版本号", config.Version)
 	// 初始化任务
 	server.InitTask()
 	// 初始化http
