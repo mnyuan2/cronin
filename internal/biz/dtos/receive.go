e62de84a8a14dc4b924d3e1697a067566885a2f0
王立国 <qwasop@qq.com>
2024-11-18 22:06:45 +0800

    receive 接收功能1期+2

---
+++ internal/biz/dtos/receive.go
@@ -0,0 +1,16 @@
+package dtos
+
+type ReceiveWebHook struct {
+	User    string              `json:"user"`
+	Type    string              `json:"type"`
+	Event   string              `json:"event"`
+	Dataset []map[string]string `json:"dataset"`
+}
+
+type ReceiveWebHookData struct {
+	Owner   string `json:"owner,omitempty"`
+	Repo    string `json:"repo,omitempty"`
+	Number  string `json:"number,omitempty"`
+	Type    string `json:"type,omitempty"`
+	Service string `json:"service,omitempty"`
+}
